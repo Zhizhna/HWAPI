ef5085643085010a7deebe2e9e36909e1024a90c
govor <govorinsasha0@gmail.com>
2024-06-14 00:40:53 +0200

    Done

--- src/main/java/com/aleks/sky/Employee.java
+++ src/main/java/com/aleks/sky/Employee.java
@@ -1,8 +1,5 @@
 package com.aleks.sky;
-
-import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
@@ -17,36 +14,23 @@ class Employee {
 
         @Override
         public Employee getEmployeeWithMinSalary(int department) {
-            return employees.values()
-                    .stream()
-                    .filter(employee -> employee.getDepartment()== department)
-                    .min(Comparator.comparing(Employee::getSalary))
-                    .orElse(null);
+            return employees.values().stream().filter(employee -> employee.getDepartment() == department).min(Comparator.comparing(Employee::getSalary)).orElse(null);
         }
 
         @Override
         public Employee getEmployeeWithMaxSalary(int department) {
-            return employees.values()
-                    .stream()
-                    .filter(employee -> employee.getDepartment() == department)
-                    .max(Comparator.comparing(Employee::getSalary))
-                    .orElse(null);
+            return employees.values().stream().filter(employee -> employee.getDepartment() == department).max(Comparator.comparing(Employee::getSalary)).orElse(null);
         }
 
         @Override
         public List<Employee> getAllEmployees(int department) {
-            return employees.values()
-                    .stream()
-                    .filter(employee -> employee.getDepartment() == department)
-                    .toList();
+            return employees.values().stream().filter(employee -> employee.getDepartment() == department).toList();
         }
 
         @Override
         public Map<String, Employee> getEmployees() {
 
-            return employees.values()
-                    .stream()
-                    .collect(Collector.grouping(Employee::getDepartment
+            return employees.values().stream().collect(Collector.grouping(Employee::getDepartment
         }
 
     }
